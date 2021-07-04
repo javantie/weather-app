3c0fa08e4ec8a4e450253b305d9e19041a22e928
Akeem C <javantieclarke@gmail.com>
2021-07-04 15:40:57 -0400

    updated main display

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var inputField = document.querySelector("input");
 var mainTempEl = document.getElementById("main-temp");
 var mainWindEl = document.getElementById("main-wind");
 var mainHumid = document.getElementById("main-humid");
+var mainUviEl = document.getElementById("main-uv");
 var displayHeading = document.getElementById("display-heading");
 var history = document.getElementById("history");
 var hsDsp = document.getElementById("hs-dsp");
@@ -10,11 +11,8 @@ var forcastArea = document.querySelector(".forecast");
 currentCity = "wolcott";
 var apiBase = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiKey = "&units=imperial&appid=6260169909dd4d4630bd110c87fff970";
-var mainDate = document.getElementById("main-date")
-
-var date = moment().format("MM/D/YYYY")
-console.log(date)
-
+var mainDate = document.getElementById("main-date");
+var date = moment().format("MM/D/YYYY");
 
 // Fetched Data for current weather information
 fetch(apiBase + currentCity + apiKey)
@@ -22,21 +20,22 @@ fetch(apiBase + currentCity + apiKey)
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
 
     //Current Location Weather Display
     mainTempEl.textContent = "Temp: " + data.main.temp + "° F";
     mainWindEl.textContent = "Wind: " + data.wind.speed + " MPH";
     mainHumid.textContent = "Humidity: " + data.main.humidity + "%";
     displayHeading.textContent = data.name;
-    mainDate.textContent = date
+    mainDate.textContent = date;
     var disIcon = document.createElement("img");
     disIcon.setAttribute(
       "src",
       "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
     );
     displayHeading.append(disIcon);
-  });
+    showUV(data.coord.lat, data.coord.lon);
+});
 
 searchButtonEl.addEventListener("click", function (event) {
   city = inputField.value;
@@ -72,6 +71,26 @@ var getResults = function () {
       oneCallApi(data.coord.lat, data.coord.lon);
     });
 };
+///Funtion to show UVI for Current Day
+var showUV = function(lat, lon){
+  var oneCallBase =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+  lat +
+  "&lon=" +
+  lon +
+  "&exclude=hourly,minutely,alerts" +
+  apiKey;
+fetch(oneCallBase)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    mainUviEl.textContent = data.current.uvi;
+    console.log(data);
+    getForcastResults(data);
+  });
+
+}
 
 //One call api with all the data both current and daily
 var oneCallApi = function (lat, lon) {
@@ -87,6 +106,7 @@ var oneCallApi = function (lat, lon) {
       return response.json();
     })
     .then(function (data) {
+      mainUviEl.textContent = "UV Index: " + data.current.uvi;
       console.log(data);
       getForcastResults(data);
     });
@@ -94,7 +114,11 @@ var oneCallApi = function (lat, lon) {
 
 ///Function for setting 5 day forcast
 var getForcastResults = function (data) {
+  forcastArea.innerHTML = "";
   for (let i = 0; i < 5; i++) {
+    var castdate = moment().add(i, "days").format("MM/D/YYYY");
+    console.log(castdate);
+
     var castBlock = document.createElement("div");
     castBlock.setAttribute("class", "block");
     var forecastDateEl = document.createElement("h3");
@@ -107,10 +131,10 @@ var getForcastResults = function (data) {
     iconEl.setAttribute(
       "src",
       "http://openweathermap.org/img/w/" +
-        data.daily[0].weather[0].icon +
+        data.daily[i].weather[0].icon +
         ".png"
     );
-    forecastDateEl.textContent = date;
+    forecastDateEl.textContent = castdate;
     tempEl.textContent = "Temp: " + data.daily[i].temp.day + "° F";
     windEl.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
     humidityEl.textContent = "Humidity: " + data.daily[i].humidity + "%";
@@ -134,9 +158,8 @@ var saveSeachData = function () {
 //Load data from local storage
 var loadData = function () {
   oldData = JSON.parse(localStorage.getItem("search")) || [];
-  console.log(oldData);
+  //console.log(oldData);
   for (let i = 0; i < oldData.length; i++) {
-    console.log(oldData[i].text);
     search = document.createElement("p");
     search.setAttribute("class", "dsp");
     search.textContent = oldData[i].text;
@@ -153,7 +176,7 @@ hsDsp.addEventListener("click", function () {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      //  console.log(data);
       mainTempEl.textContent = "Temp: " + data.main.temp + "° F";
       mainWindEl.textContent = "Wind: " + data.wind.speed + " MPH";
       mainHumid.textContent = "Humidity: " + data.main.humidity + "%";
